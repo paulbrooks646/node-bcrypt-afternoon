c65ca4a52a48d3ec21feebbb9eb65891e91b39d3
paulbrooks646 <paulbrooks646@gmail.com>
2020-06-02 19:22:51 -0600

    step6

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session')
+const massive = require('massive');
+const authCtrl = require('./controllers/authController.js')
+const treasureCtrl = require('./controllers/treasureController')
+
+const PORT = 4000;
+
+const {CONNECTION_STRING, SESSION_SECRET} = process.env
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/auth/register', authCtrl.register);
+app.post('/auth/login', authCtrl.login);
+app.get('/auth/logout', authCtrl.logout);
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+    console.log('db connected');
+  });
+  
+  app.use(
+    session({
+      resave: true,
+      saveUninitialized: false,
+      secret: SESSION_SECRET,
+    })
+  );
+
+  app.listen(PORT, () => {
+    console.log(`My name is port ${PORT}, my name is my passport, verify me.`)})
